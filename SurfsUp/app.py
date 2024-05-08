b1e0edb0d02ebddfe7d2e8c17081df0c6d8ab4c9
Andreanimako <andreanimako@gmail.com>
2024-05-08 08:47:43 -0400

    climate app code added

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -50,7 +50,7 @@ def Welcome():
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/start<br/>"
         f"/api/v1.0/start/end<br/>"
-        f"To determine the tempearture values for a given start and end date, enter the date in the format- yyyy-mm-dd<br/> "
+        f"To determine the tempearture values for a given 'start' and 'end' date, enter the date in the format- yyyy-mm-dd<br/> "
         f"for example- /api/v1.0/2017-08-08/2018-08-08"
     )
 
@@ -78,6 +78,7 @@ def precipitation():
     sel = [Measurement.date, Measurement.prcp]
     results = session.query(*sel).filter(Measurement.date >= query_date).all()
 
+    #closing session
     session.close()
 
     #populating the precipitation dictionary
@@ -103,6 +104,7 @@ def stations():
     #creating list
     station_list = list(np.ravel(results))
 
+    #returning the json version of the data    
     return jsonify(station_list)
 
 
@@ -115,26 +117,26 @@ def tobs():
     session= Session(engine)
 
     #determining the most active station
-    # Listing the stations and their counts in descending order.
+    # Listing the stations and their counts in descending order and selecting the first, which is the most active station
     top_station = session.query(Measurement.station, func.count(Measurement.station)).\
                     group_by(Measurement.station).\
                     order_by (func.count(Measurement.station).desc()).first()
     
-    # Extract the station ID from the result tuple
+    # Extract the station ID from the result above
     id_top_station= top_station[0]
     
     
-    #querying the date and preciptation data for last 12 months
+    #querying the date and temperature data for last 12 months
     # Starting from the most recent data point in the database. 
 
     last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
     last_date = dt.datetime.strptime(last_date[0], '%Y-%m-%d').date()
 
-    # Query the last 12 months of temperature observation data for this station and plot the results as a histogram
+    # Query the last 12 months of temperature observation data
     query_date = last_date - dt.timedelta(days=365)
     query_date
 
-    # query to retrieve the temperature
+    # query to retrieve the temperature data over the last 12 moonths for the most active station
     sel = [Measurement.date, Measurement.tobs]
     results = session.query(*sel).filter(Measurement.date >= query_date).filter(Measurement.station == id_top_station).all()
 
@@ -148,13 +150,13 @@ def tobs():
 
 
 @app.route("/api/v1.0/<start>")
-@app.route("/api/v1.0/<start/end>")
+@app.route("/api/v1.0/<start>/<end>")
 def start_end(start, end = None):
 
     #creating a session link from python to DB
     session= Session(engine)
 
-    #querying list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
+    #querying list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range
     if end:
         results= session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).\
                 filter(Measurement.date >= start).filter(Measurement.date <= end).all()
@@ -163,6 +165,7 @@ def start_end(start, end = None):
         results= session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).\
                 filter(Measurement.date >= start).all()
     
+    #populating temp_values dictionary
     temp_values = []
     for min_temp, avg_temp, max_temp in results:
         temp_values.append({
@@ -173,11 +176,10 @@ def start_end(start, end = None):
             "max_temperature": max_temp
         })
 
+    #returning the json version of the data    
     return jsonify(temp_values)
 
         
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
